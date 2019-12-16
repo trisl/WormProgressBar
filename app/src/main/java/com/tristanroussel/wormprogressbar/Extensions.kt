09ee6732489ef35bf14a38fa7b3f0700be920d61
Tristan Roussel <tristan.roussel@frichti.co>
2019-12-16 15:33:30 +0100

    Create a basic progress bar with configurable color.

---
+++ app/src/main/java/com/tristanroussel/wormprogressbar/Extensions.kt
@@ -0,0 +1,12 @@
+package com.tristanroussel.wormprogressbar
+
+import android.content.Context
+import androidx.annotation.ColorInt
+import androidx.annotation.ColorRes
+import androidx.core.content.ContextCompat
+
+/**
+ * Created by Tristan Roussel on 2019-12-16.
+ */
+@ColorInt
+fun Context.color(@ColorRes resource: Int): Int = ContextCompat.getColor(this, resource)
---
+++ app/src/main/java/com/tristanroussel/wormprogressbar/WormConfiguration.kt
@@ -0,0 +1,9 @@
+package com.tristanroussel.wormprogressbar
+
+/**
+ * Created by Tristan Roussel on 2019-12-16.
+ */
+data class WormConfiguration(
+        var primaryColor: Int = -1,
+        var secondaryColor: Int = -1
+)
