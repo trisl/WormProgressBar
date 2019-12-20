d1b5ed1449428c6822b392a57c0c4626f2716574
Tristan Roussel <tristan.roussel@frichti.co>
2019-12-20 11:27:47 +0100

    Create README.

---
+++ README.md
@@ -0,0 +1,12 @@
+# WormProgressBar
+
+A worm like animation of a progress bar in Android
+
+[![](https://jitpack.io/v/trisl/WormProgressBar.svg)](https://jitpack.io/#trisl/WormProgressBar)
+[![API](https://img.shields.io/badge/API-21%2B-brightgreen.svg?style=flat)](https://android-arsenal.com/api?level=21)
+
+![Worm](gifs/worm.gif)
+
+## Usage
+## Setup
+## License
--- app/src/main/java/com/tristanroussel/wormprogressbar/MainActivity.kt
+++ app/src/main/java/com/tristanroussel/wormprogressbar/MainActivity.kt
@@ -1,32 +1,37 @@
 package com.tristanroussel.wormprogressbar
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.tristanroussel.worm.WormProgressBar
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+    
+    private lateinit var startButton: View
+    private lateinit var pauseButton: View
+    private lateinit var completeButton: View
+    
     private lateinit var wormProgressBar: WormProgressBar
-
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        
+        startButton = button_start
+        pauseButton = button_pause
+        completeButton = button_complete
+        
         wormProgressBar = worm_view
-
+        
         wormProgressBar.initAnimation()
     }
-
-    override fun onResume() {
-        super.onResume()
-
-        wormProgressBar.start()
-    }
-
-    override fun onPause() {
-        super.onPause()
-
-        wormProgressBar.pause()
+    
+    override fun onStart() {
+        super.onStart()
+        
+        startButton.setOnClickListener { wormProgressBar.start() }
+        pauseButton.setOnClickListener { wormProgressBar.pause() }
+        completeButton.setOnClickListener { wormProgressBar.complete() }
     }
 }
